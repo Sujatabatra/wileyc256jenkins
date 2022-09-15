476c4f81362ee6e1c0ac6dc2705d3e518f309f1b
Sujatabatra <sujatabatra@hotmail.com>
2022-09-15 09:31:45 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
   public static void main(String args[]){
     System.out.println("Hello from Jenkins By Java Code.......");
+    while(true){
   }
+    System.out.println("Good Bye from main!");
 }
